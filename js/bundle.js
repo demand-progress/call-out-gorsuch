075ed01d18b02ffe848647256e10b2c19b8dbe61
Tibet Sprague <tibet.sprague@gmail.com>
2017-03-23 12:21:20 -0700

    Get rid of Creative Commons for now

--- js/bundle.js
+++ js/bundle.js
@@ -899,80 +899,6 @@
 	    }
 	});
 
-	var CreativeCommons = React.createClass({
-	    displayName: 'CreativeCommons',
-
-	    render: function render() {
-	        return React.createElement(
-	            'div',
-	            { className: 'creative-commons' },
-	            'Trump photo (edited) via ',
-	            React.createElement(
-	                'a',
-	                { href: 'https://commons.wikimedia.org/wiki/File%3ADonald_Trump_(16493063167).jpg', target: '_blank' },
-	                'Michael Vadon'
-	            ),
-	            ' under a ',
-	            React.createElement(
-	                'a',
-	                { href: 'http://creativecommons.org/licenses/by-sa/2.0', target: '_blank' },
-	                'CC BY-SA 2.0'
-	            ),
-	            ' license.',
-	            React.createElement('br', null),
-	            'Sessions photo (edited) via ',
-	            React.createElement(
-	                'a',
-	                { href: 'https://commons.wikimedia.org/wiki/File%3AJeff_Sessions_by_Gage_Skidmore.jpg', target: '_blank' },
-	                'Gage Skidmore'
-	            ),
-	            ' under a ',
-	            React.createElement(
-	                'a',
-	                { href: 'http://creativecommons.org/licenses/by-sa/3.0', target: '_blank' },
-	                'CC BY-SA 3.0'
-	            ),
-	            ' license.',
-	            React.createElement('br', null),
-	            'Tillerson photo (edited) via ',
-	            React.createElement(
-	                'a',
-	                { href: 'https://www.flickr.com/photos/worldeconomicforum/3488866258/', target: '_blank' },
-	                'World Economic Forum'
-	            ),
-	            ' under a ',
-	            React.createElement(
-	                'a',
-	                { href: 'https://creativecommons.org/licenses/by-nc-sa/2.0/', target: '_blank' },
-	                'CC BY-NC-SA 2.0'
-	            ),
-	            ' license.',
-	            React.createElement('br', null),
-	            'Ross photo (edited) via Cyprus Business Press under a ',
-	            React.createElement(
-	                'a',
-	                { href: 'https://creativecommons.org/licenses/by-nc-nd/3.0/', target: '_blank' },
-	                'CC BY-NC-ND 3.0'
-	            ),
-	            ' license.',
-	            React.createElement('br', null),
-	            'Pruitt photo (edited) via ',
-	            React.createElement(
-	                'a',
-	                { href: 'https://www.flickr.com/photos/gageskidmore/16503867219/in/photolist-r9oCJP-rqQHPX-r9heYS-eac2U8-ERurNv-rqKgqb-E3i1cr-p1nWhb-nsVqj3-nbGwQY-nbGtUu-nbGqz2-nbGnqM-nteJif-nte9nu-r9g9xm-nbH1aX-qu4gpp-nbH3D4-nsV2gZ-qu49w6-2QuBHv-nbGEPe-9mXmBW-6LaCys-nsUuWa-nr9pGw-nbGtX2-nsVpdW-roxxSu-nbGWWL-nbGpWD-r7w9jM-roxzyL-9mXnoy-nsVe6S-ntbZYR-8sDtvi-r9hceU-9mUjLk-nbGZRS-nbGJ5d-nteqzj-nuY8Px-8vGwz2-nsUsTT-8sGwmo-nsVi6A-roxGE9-nr9QVs', target: '_blank' },
-	                'Gage Skidmore'
-	            ),
-	            ' under a ',
-	            React.createElement(
-	                'a',
-	                { href: 'https://creativecommons.org/licenses/by-sa/2.0/', target: '_blank' },
-	                'CC BY-SA 2.0'
-	            ),
-	            ' license.'
-	        );
-	    }
-	});
-
 	var Social = React.createClass({
 	    displayName: 'Social',
 
@@ -1053,8 +979,7 @@
 	            React.createElement(Form, null),
 	            React.createElement(Social, null),
 	            React.createElement(Organizations, null),
-	            React.createElement(Contact, null),
-	            React.createElement(CreativeCommons, null)
+	            React.createElement(Contact, null)
 	        );
 	    },
 
